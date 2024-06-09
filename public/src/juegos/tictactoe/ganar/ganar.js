015b321d44c2a75c7ac0a42d2bd21358537f0b6e
4Lcalde <aitorjimeneztecno@gmail.com>
2024-06-09 17:26:15 +0200

    v.01

--- public/src/juegos/tictactoe/ganar/ganar.js
+++ public/src/juegos/tictactoe/ganar/ganar.js
@@ -1,4 +1,5 @@
 import { estadoJuego } from '../../../data/datosTicTacToe'
+import { mostrarModal } from '../mostrarModal.js/mostrarModal'
 export const ganar = (ganador, a, b, c) => {
   estadoJuego.partidaActiva = false // Desactivar la partida
   estadoJuego.estadoJuego = 'pausa'
