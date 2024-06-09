f4e8117edbec03e80631e82198019f103ea58ef0
4Lcalde <aitorjimeneztecno@gmail.com>
2024-06-09 18:24:40 +0200

    rf

--- public/src/components/header/header.js
+++ public/src/components/header/header.js
@@ -13,7 +13,6 @@ export const headerCreator = (name) => {
   const logo = document.createElement('img')
   logo.id = 'logo'
   logo.src = '/assets/poker.png'
-
   logo.alt = 'Logo'
 
   logo.addEventListener('click', () => {
@@ -25,26 +24,28 @@ export const headerCreator = (name) => {
   menuToggle.classList.add('toggle')
   menuToggle.textContent = 'Elegir juego  ▽'
 
-  menuToggle.addEventListener('click', () => {
-    navGames.classList.toggle('visible')
-  })
-
   const navGames = document.createElement('nav')
   navGames.className = 'nav-games'
   const ulGames = document.createElement('ul')
   ulGames.className = 'ul-games'
+
+  const juegos = [
+    { name: 'Juego 1', function: () => console.log('Juego 1') },
+    { name: 'Juego 2', function: () => console.log('Juego 2') }
+    // Add more games as needed
+  ]
 
   for (const juego of juegos) {
     const li = document.createElement('li')
     const a = document.createElement('a')
     a.textContent = juego.name
 
-    a.addEventListener('click', juego.function)
-
-    li.addEventListener('click', () => {
+    a.addEventListener('click', (event) => {
+      event.preventDefault()
       juego.function()
       navGames.classList.toggle('visible')
     })
+
     li.appendChild(a)
     ulGames.appendChild(li)
   }
@@ -54,6 +55,7 @@ export const headerCreator = (name) => {
   const imgUser = document.createElement('img')
   imgUser.className = 'img-user'
   imgUser.src = '/assets/user.png'
+  imgUser.alt = 'User'
   const nameUser = document.createElement('p')
   nameUser.textContent = name
 
@@ -62,14 +64,19 @@ export const headerCreator = (name) => {
   buttonLogOut.classList.add('button-logout')
   buttonLogOut.id = 'logOut'
 
-  divUser.addEventListener('click', () =>
+  divUser.addEventListener('click', () => {
     buttonLogOut.classList.toggle('abierto')
-  )
+  })
 
   divUser.append(nameUser, imgUser, buttonLogOut)
 
   navGames.appendChild(ulGames)
   header.append(logo, navGames, menuToggle, divUser)
-  document.body.insertBefore(header, app, divUser)
+  document.body.insertBefore(header, app)
+
+  menuToggle.addEventListener('click', () => {
+    navGames.classList.toggle('visible')
+  })
+
   logOutAction()
 }
